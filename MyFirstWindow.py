e8084f05ca332f999203e67007052ad603c94f5d
nikolaj <strashevskiynn@g,ail.com>
2021-04-24 10:17:57 +0300

    first window

--- MyFirstWindow.py
+++ MyFirstWindow.py
@@ -1,2 +1,24 @@
 import sys
-from PySide2 import QtWidgets
+# from PySide2 import QtWidgets
+from PyQt5 import QtWidgets
+
+
+class MyFirstWindow(QtWidgets.QWidget):
+
+    def __init__(self, parent=None):
+        QtWidgets.QWidget.__init__(self, parent)
+
+        layout = QtWidgets.QVBoxLayout()
+        button = QtWidgets.QPushButton('Кнопка')
+
+
+        layout.addWidget(button)
+
+        self.setLayout(layout)
+
+
+if __name__ == '__main__':
+    app = QtWidgets.QApplication(sys.argv)
+    myform = MyFirstWindow()
+    myform.show()
+    sys.exit(app.exec_())
