1ae95f2fc63386d2f15b8fb57a76823ef59e39e1
nikolaj <strashevskiynn@g,ail.com>
2021-05-28 16:27:07 +0300

    progress_bar

--- progress_bar.py
+++ progress_bar.py
@@ -12,25 +12,29 @@ class MyFirstProgressBar(QtWidgets.QWidget):
         self.initUi()
 
     def initUi(self):
+        cpu_percent_list = psutil.cpu_percent(interval=1, percpu=True)
         self.layout = QtWidgets.QHBoxLayout()
         for cpu_num in range(psutil.cpu_count()):
             self.layout.addWidget(cpu_counting(cpu_num+1))
         self.setLayout(self.layout)
         self.cpu_threed.cpu_count.connect(self.update_pb, QtCore.Qt.QueuedConnection)
 
-    def update_pb(self, cpu_percent_list):
+    def update_pb(self):
         cpu_percent_list = psutil.cpu_percent(interval=1, percpu=True)
         for w in range(self.layout.count()):
             l1 = self.layout.itemAt(w).widget()
             l2 = l1.layout().itemAt(0).widget()
             l2.setValue(cpu_percent_list[w])
+            l3 = l1.layout().itemAt(1).widget()
+            l3.setText(str(f'загрузка ядра {cpu_percent_list[w]} %'))
+
 
 
 class cpu_counting(QtWidgets.QWidget):
     def __init__(self, cpu_num, parent=None):
         super(cpu_counting, self).__init__(parent)
-
         layout = QtWidgets.QVBoxLayout()
+        self.qlablePercent = QtWidgets.QLabel(f'')
         self.qlable = QtWidgets.QLabel(f'CPU {cpu_num}')
         self.qlable.setAlignment(QtCore.Qt.AlignHCenter)
         self.prBar = QtWidgets.QProgressBar()
@@ -40,6 +44,7 @@ class cpu_counting(QtWidgets.QWidget):
         self.prBar.setTextVisible(True)
         self.prBar.setSizePolicy(QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Expanding)
         layout.addWidget(self.prBar)
+        layout.addWidget(self.qlablePercent)
         layout.addWidget(self.qlable)
 
         self.setLayout(layout)
